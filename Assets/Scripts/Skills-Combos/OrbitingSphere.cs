e38a2f11c398fa4fc549a459e050c00a033a4eb9
DiegoDavis <diegodvsmelo@gmail.com>
2025-12-17 01:32:10 -0300

    feat: air sound implemented

--- Assets/Scripts/Skills-Combos/OrbitingSphere.cs
+++ Assets/Scripts/Skills-Combos/OrbitingSphere.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class OrbitingSphere : MonoBehaviour
 {
     public int damage;
+    public AudioClip audioClip;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
+            SoundEffectsManager.Instance.PlaySFXClip(audioClip, transform, 0.8f);
             EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
             if (enemyHealth != null)
             {
