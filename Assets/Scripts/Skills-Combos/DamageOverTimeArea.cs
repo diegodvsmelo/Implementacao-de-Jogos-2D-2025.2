2b6c7ed1bfb72804325f73d908060c63d1f6a245
DiegoDavis <diegodvsmelo@gmail.com>
2025-12-17 01:01:05 -0300

    feat: earthsound implemented

--- Assets/Scripts/Skills-Combos/DamageOverTimeArea.cs
+++ Assets/Scripts/Skills-Combos/DamageOverTimeArea.cs
@@ -1,42 +1,82 @@
-using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageOverTimeArea : MonoBehaviour
 {
     [Header("Configuração do Dano")]
-    public int damagePerTick;
-    public float timeBetweenTicks;
-    public float duration;
-    public AudioClip[] damageAudio;
+    public int damagePerTick = 10;
+    public float timeBetweenTicks = 0.5f;
+    public float duration = 3f;
 
-    private BoxCollider2D areaCollider;
+    [Header("Áudio")]
+    [SerializeField] private AudioClip[] tickSounds; 
+    [Range(0f, 1f)] [SerializeField] private float tickVolume = 0.5f;
 
-    void Start()
+    private void Start()
     {
-        areaCollider = GetComponent<BoxCollider2D>();
-
-        StartCoroutine(DamagePulse());
+        StartCoroutine(DamageRoutine());
         
         Destroy(gameObject, duration);
     }
 
-    private IEnumerator DamagePulse()
+    private IEnumerator DamageRoutine()
     {
-        while(true)
-        {
-            Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(transform.position, areaCollider.size, transform.eulerAngles.z);
 
-            foreach (Collider2D enemy in hitEnemies)
+        float timer = 0f;
+        
+        while (timer < duration)
+        {
+            ApplyAreaDamage();
+            PlayTickSound();
+
+            yield return new WaitForSeconds(timeBetweenTicks);
+            timer += timeBetweenTicks;
+        }
+    }
+
+    private void ApplyAreaDamage()
+    {
+        Vector2 center = transform.position;
+        Vector2 size = Vector2.one;
+
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        if (collider != null)
+        {
+            center = (Vector2)transform.position + collider.offset;
+            size = collider.size;
+        }
+
+        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(center, size, 0f);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            if (enemy.CompareTag("Enemy"))
             {
-                EnemyHealth health = enemy.GetComponent<EnemyHealth>();
-                if (health != null)
+                var enemyHealth = enemy.GetComponent<EnemyHealth>();
+                if (enemyHealth != null)
                 {
-                    health.TakeDamage(damagePerTick);
-                    SoundEffectsManager.Instance.PlayRandomSFXClip(damageAudio, transform, 0.4f);
+                    enemyHealth.TakeDamage(damagePerTick);
                 }
             }
-            yield return new WaitForSeconds(timeBetweenTicks);
+        }
+    }
+
+    private void PlayTickSound()
+    {
+        if (tickSounds != null && SoundEffectsManager.Instance != null)
+        {
+            SoundEffectsManager.Instance.PlayRandomSFXClip(tickSounds, transform, tickVolume);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        if (collider != null)
+        {
+            Gizmos.DrawWireCube((Vector2)transform.position + collider.offset, collider.size);
         }
     }
 }
